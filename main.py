2dd34a4eda74cbf217b9c4fffbf01e20eedc73da
anshuman-sparsh <anshumansparsh@gmail.com>
2026-03-26 14:23:20 +0530

    Updated Driver and dependents

--- main.py
+++ main.py
@@ -18,7 +18,11 @@ def check_movie():
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
 
-    driver = webdriver.Chrome(options=options)
+    from selenium.webdriver.chrome.service import Service
+    from webdriver_manager.chrome import ChromeDriverManager
+
+    service = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=service, options=options)
     driver.get(URL)
 
     time.sleep(7)  # wait for page to load
