4d7aca85e146199e7f833a278b8ebeab75656b35
anshuman-sparsh <anshumansparsh@gmail.com>
2026-04-03 13:46:49 +0530

    Changed platform

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 
 
-URL = "https://in.bookmyshow.com/explore/movies-muzaffarpur"
+URL = "https://www.district.in/movies/muzaffarpur-movie-tickets"
 
 
 def check_movie():
